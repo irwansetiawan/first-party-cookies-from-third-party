b10d123046c67259e3f9fd07ad1452f4323cd913
Irwan Setiawan <irwan@irwansetiawan.com>
2024-08-11 00:20:21 +0800

    Make subnet public with internet gateway

--- terraform/ec2.tf
+++ terraform/ec2.tf
@@ -31,11 +31,30 @@ resource "aws_vpc" "main" {
   enable_dns_hostnames = true
 }
 
+resource "aws_main_route_table_association" "main" {
+  vpc_id         = aws_vpc.main.id
+  route_table_id = aws_route_table.main.id
+}
+
 resource "aws_subnet" "main" {
   tags = { Name = "first-party-cookies" }
   vpc_id     = aws_vpc.main.id
   cidr_block = "10.0.0.0/24"
   map_public_ip_on_launch = true
+}
+
+resource "aws_route_table" "main" {
+  tags = { Name = "first-party-cookies" }
+  vpc_id = aws_vpc.main.id
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.main.id
+  }
+}
+
+resource "aws_internet_gateway" "main" {
+  tags = { Name = "first-party-cookies" }
+  vpc_id = aws_vpc.main.id
 }
 
 resource "aws_vpc_security_group_ingress_rule" "allow_http_ipv4" {
